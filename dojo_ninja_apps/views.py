107fcdb83b5d08a5dee9f3040446ffc19e0c3f16
Michael.Erickson <ylnorthville@gmail.com>
2020-10-22 15:26:22 -0400

    I have added all the features needed

--- dojo_ninja_apps/views.py
+++ dojo_ninja_apps/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from dojo_ninja_apps.models import *
 
-# Create your views here.
+def index(request):
+    context = {
+        'dojos': Dojo.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def dojo(request):
+    Dojo.objects.create(
+        name=request.POST['name'],
+        city=request.POST['city'],
+        state=request.POST['state'],
+    )
+    return redirect('/')
+
+def ninja(request):
+    Ninja.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        dojo_id=request.POST['dojo'],
+    )
+    return redirect('/')
